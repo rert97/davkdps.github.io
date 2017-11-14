7eb7e26e2813f784d3c2c21758ee467e41144df6
lzzy12 <jhashivam2020@gmail.com>
2017-11-14 09:02:20 -0800

    More optimisations for mobile device

---
+++ nav.js
@@ -0,0 +1,3 @@
+if (screen.width <= 1300)
+{document.write(" <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br> <br/> This is wrong");
+}
