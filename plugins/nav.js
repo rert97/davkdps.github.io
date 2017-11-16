d88fcdb4d6143182f9f019fd20b17c0b2114c96f
lzzy12 <jhashivam2020@gmail.com>
2017-11-16 02:41:44 +0000

    index.html : Add rainbow preloader
    Signed-off-by: lzzy12 <jhashivam2020@gmail.com>

--- plugins/nav.js
+++
@@ -1,3 +0,0 @@
-if (screen.width <= 1300)
-{document.write(" <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br> <br/> This is wrong");
-}
---
+++ plugins/preloader.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+	setTimeout(function() { $("#preloader").fadeOut(2000); },1000);
+	$("#body-hider").fadeIn(2100);
+});
